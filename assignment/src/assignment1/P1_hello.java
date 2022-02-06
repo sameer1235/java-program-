4f3286290965b7a47e71b289a7affc0903606282
Good Luck <sam123msr@gmail.com>
2022-02-07 00:00:48 +0530

    Assignment1 committed

---
+++ assignment/src/assignment1/P1_hello.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+public class P1_hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ assignment/src/assignment1/P2_addition.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class P2_addition {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("addition of A and B is : "+c);
+
+	}
+
+}
